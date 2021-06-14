0080324f508e10cab5a0a1d913c361ddac7b3765
KamilLetowski <kamilxdr96@gmail.com>
2021-06-14 10:09:45 +0200

    a

--- providers/user/index.ts
+++ providers/user/index.ts
@@ -5,8 +5,9 @@ import useState from './useState';
 export type Action = { type: 'SET_USER_DATA'; payload: State };
 export type State = UserLogged;
 
-const { Provider, useStateContext } = buildContext(useState);
-export const useUserContext = useStateContext;
+const {
+	Provider: UserProvider,
+	useStateContext: useUserContext,
+} = buildContext(useState);
 
-const UserProvider = Provider;
-export default UserProvider;
+export { UserProvider as default, useUserContext };
